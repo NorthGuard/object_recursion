51f4e8e66b08b9f945c5ce73f509c0abc8d70082
Jepno <jepno@dtu.dk>
2017-06-02 12:01:22 +0200

    Script and README

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # type_recursive
+Allows for recursively determining types of various Python objects.
+
+
+### Usage
+Calling `type_recursive()` on an object returns a string describing the type and contained types of that object.
+```python
+from type_recursive import type_recursive
+
+print(type_recursive([1, None, "str"]))
+# Prints: list[int|None|str]
+
+print(type_recursive((False, [" "])))
+# Prints: tuple[bool,list[str]]
+```
+
+The method can also handle user-defined classes.
+
+
+### Test Script
+
+The main-block of `type_recursive.py`, runs the method on a list of objects and creates the following print.
+On the left there is the string-representation of each object as returned by `repr(obj)`, on the right is the 
+recursive type returned by `type_recursive(obj)`.
+
+```
+1                                                  : int
+2.3                                                : float
+None                                               : None
+False                                              : bool
+'hello'                                            : str
+[1, 2, 3]                                          : list[int]
+['a', 'b']                                         : list[str]
+[1, 'h']                                           : list[int|str]
+(False, 1, '2')                                    : tuple[bool,int,str]
+{1.2, 2.3, 3.4}                                    : set[float]
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]                  : list[list[int]]
+[(1, 'a'), (2, 'b')]                               : list[tuple[int,str]]
+{1: 'b', 2: 'c'}                                   : dict[int: str]
+{1: 'b', 2: None}                                  : dict[int: None|str]
+[<__main__.Foo object at 0x0000028E3861FC50>]      : list[Foo]
+[<function bar at 0x0000028E3857C488>]             : list[callable()]
+Bob(a=1, b=2, c=3)                                 : Bob[int,int,int]
+array([1, 2, 3])                                   : np.1darray[int32]
+array([['1', 'b'], ['3', '4']], dtype='<U11')      : np.2darray[str_]
+
+```
+
+
+### Options
+
+##### Symbols
+
+The symbols below are used in the representation, but can all be changed with optional arguments to 
+`type_recursive()`.  
+
+`[]` Delimiters of types contained within another object (fx. lists, tuples or dictionaries).  
+`|` Indicates that a mutable object can have various interchangeable types (fx. lists).  
+`,` Indicates that an immutable object can have various ordered types (fx. tuples).   
+`:` Symbol for the mapping from one type to another (fx. dictionaries).
+
+##### Numpy dimensions
+
+Default setting is to show how many dimensions a numpy array has. For example a one-dimensional array will have type 
+`np.1darray` while a two-dimentional matrix will have type `np.2darray`. This numbering can be turned off by 
+passing `show_numpy_dimensions=False` to `type_recursive()`, in which the two types would both be `np.ndarray`.
+
+##### Sampling
+
+`type_recursive()` by default recursively goes through all objects within an object. 
+With large data-structures this can get time-consuming. Therefore one can pass `sampling=X` to `type_recursive()`,
+where `X` is an integer. With this argument, `type_recursive()` takes random `X` samples from any 
+list/tuple/iterable etc. and creates a type-string based on those sample. 
+This is of cause much faster, but one can not be sure that every nested type is reported (for example if
+a single `None`-value is hidden between thousands of integers).
---
+++ type_recursive.py
@@ -0,0 +1,186 @@
+import random
+import numpy as np
+from collections import namedtuple
+from typing import Iterable, Dict, Callable, Tuple
+
+
+def _type_recursive_iterable(obj, sampling, or_divider, l, r):
+    # Get type-strings of elements in iterable (recursive)
+    if sampling is None:
+        tps = list(set(type_recursive(e) for e in obj))
+    else:
+        tps = list(set(type_recursive(e) for e in random.sample(list(obj), sampling)))
+
+    # Get name of object
+    name = type(obj).__name__
+
+    # String for output
+    string = f"{name}{l}" + or_divider.join(tps) + r
+
+    return string
+
+
+def _type_recursive_dict(obj, sampling, or_divider, map_divider, l, r):
+    # Get type-strings of keys and values (recursive)
+    if sampling is None:
+        tps = set((type_recursive(k), type_recursive(v)) for (k, v) in obj.items())
+    else:
+        tps = set((type_recursive(k), type_recursive(v)) for (k, v) in random.sample(obj.items(), sampling))
+
+    # Get key-types and value-types
+    key_types = set(k for (k, v) in tps)
+    value_types = set(v for (k, v) in tps)
+
+    # String for keys and values
+    key_type_string = or_divider.join(key_types)
+    value_type_string = or_divider.join(value_types)
+
+    # Make final string
+    string = f"dict{l}{key_type_string}{map_divider}{value_type_string}{r}"
+
+    return string
+
+
+def _type_recursive_tuple(obj, sampling, and_divider, l, r):
+    # Get type-strings of elements in tuple (recursive)
+    if sampling is None:
+        tps = [type_recursive(e) for e in obj]
+    else:
+        tps = [type_recursive(e) for e in random.sample(list(obj), sampling)]
+
+    # Get name of object
+    name = type(obj).__name__
+
+    # Make final string
+    string = f"{name}{l}" + and_divider.join(tps) + r
+
+    return string
+
+
+def _type_recursive_numpy(obj, show_numpy_dimensions, sampling, or_divider, l, r):
+    # See if obj is numpy-primitive
+    if not isinstance(obj, np.ndarray):
+        return type(obj).__name__
+
+    # Get name of object
+    name = type(obj).__name__
+
+    # Items
+    items = obj.ravel()
+
+    # Get type-strings of elements in object (recursive)
+    if sampling is None:
+        tps = list(set(type_recursive(e) for e in items))
+    else:
+        tps = list(set(type_recursive(e) for e in random.sample(items, sampling)))
+
+    # Get number of dimensions
+    dims = len(obj.shape)
+
+    # Insert number of dimensions if needed
+    if show_numpy_dimensions:
+        if name == "ndarray":
+            name = f"np.{dims}darray"
+        else:
+            name = f"np.{name}({dims})"
+
+    # String for output
+    string = f"{name}{l}" + or_divider.join(tps) + r
+
+    return string
+
+
+def _delimiter_types(delimiter="<"):
+    if delimiter in "p()":
+        return "(", ")"
+    elif delimiter in "s[]":
+        return "[", "]"
+    elif delimiter in "c{}":
+        return "{", "}"
+    else:
+        return "<", ">"
+
+
+def type_recursive(obj, sampling=None, delimiter="[", or_divider="|", and_divider=",", map_divider=": ",
+                   show_numpy_dimensions=True):
+
+    # Get delimiters
+    l, r = _delimiter_types(delimiter=delimiter)
+
+    # Get type of object
+    t = type(obj)
+
+    # NoneType -> None
+    if obj is None:
+        return "None"
+
+    # Basic types (str is also iterable, so it must be handled here - the others could be moved to default)
+    if t in {int, str, bool, float, complex}:
+        return t.__name__
+
+    # Tuples
+    if isinstance(obj, Tuple):
+        return _type_recursive_tuple(obj, sampling=sampling, and_divider=and_divider, l=l, r=r)
+
+    # Dictionaries
+    if isinstance(obj, Dict):
+        return _type_recursive_dict(obj, sampling=sampling, or_divider=or_divider, map_divider=map_divider, l=l, r=r)
+
+    # Numpy special case
+    if t.__module__ == "numpy":
+        return _type_recursive_numpy(obj, show_numpy_dimensions=show_numpy_dimensions,
+                                     sampling=sampling, or_divider=or_divider, l=l, r=r)
+
+    # Iterables
+    if isinstance(obj, Iterable):  # This category includes objects with specific formatting like dict, tuple and str
+        return _type_recursive_iterable(obj, sampling=sampling, or_divider=or_divider, l=l, r=r)
+
+    # Functions / callable
+    if isinstance(obj, Callable):
+        return "callable()"
+
+    # Default
+    return t.__name__
+
+
+if __name__ == "__main__":
+    import re
+
+    # For making prints one-liners
+    whitespace = re.compile("[\s\n]+")
+
+    # Example classes and types
+    class Foo(object):
+        pass
+
+    def bar():
+        pass
+
+    bob = namedtuple("Bob", "a, b, c")
+    array = np.array([1, 2, 3])
+    array2 = np.array([[1, "b"], [3, 4]])
+
+    # Run a ton of examples
+    for obj in [
+        1,
+        2.3,
+        None,
+        False,
+
+        "hello",
+        [1, 2, 3],
+        ["a", "b"],
+        [1, "h"],
+        (False, 1, "2"),
+        {1.2, 2.3, 3.4},
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        [(1, 'a'), (2, 'b')],
+        {1: 'b', 2: 'c'},
+        {1: 'b', 2: None},
+        [Foo()],
+        [bar],
+        bob(1, 2, 3),
+        array,
+        array2
+    ]:
+        print("{: <50}".format(whitespace.sub(" ", repr(obj))), ":", type_recursive(obj))
